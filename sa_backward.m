f56beed7eed77c79ad4ceaba38c7bccb12087014
Peter Tso <tsopeter@ku.edu>
2023-02-13 13:08:37 -0600

    nonlinear implementation now preserves phase

--- sa_backward.m
+++ sa_backward.m
@@ -1,20 +1,12 @@
 function z = sa_backward(E, a0)
-    if (abs(E) < 0.0000001)
-        z = 0.000001;
+    g = abs(E);
+    p = angle(E);
+
+    if (g < 0.0001)
+        g = 0.0001;
     else
-        g = sa_forward(E, a0)/E;
-        num = a0 * (E^2);
-        dem = (1 + (E^2))^2;
-        if abs(dem) == 0
-            dem = 0.001;
-        end
-        a = exp(1+num/dem);
-        z = g * a;
-        if isnan(z)
-            z = 0.001;
-        end
-        if isinf(z)
-            z = 0.001;
-        end
+        g = abs(sa_forward(E, a0))/g;
+        g = g * exp(1+(a0*g*g)/(1+g^2)^2);
     end
+    z = g * exp(1i * p);
 end
--- sa_forward.m
+++ sa_forward.m
@@ -1,8 +1,8 @@
 function z = sa_forward(E, a0)
-    num = - a0/2;
-    dem = 1 + E^2;
-    if abs(dem) == 0
-        dem = 0.001;
-    end
-    z = exp(num/dem)*E;
+    % preserve phase
+    g = abs(E);
+    p = angle(E);
+
+    g = exp(-a0/2/(1+g^2));
+    z = g * exp(1i*p);
 end
