fe27b0e397990ed286582a4a68778a9a3b105b71
Peter Tso <tsopeter@ku.edu>
2023-02-14 11:13:44 -0600

    Update internal_random_amp.m

--- internal_random_amp.m
+++ internal_random_amp.m
@@ -2,7 +2,9 @@ function output = internal_random_amp(Nx, Ny)
     output = zeros(Nx, Ny);
     for i=1:1:Nx
         for j=1:1:Ny
-            output(i,j)=randn()+randn()*1j;
+            z = randn()+randn()*1i;
+            g = abs(z);
+            output(i,j)=z/g;
         end
     end
 end
