d91a71290191ecc197561902f368349fdb79e9dc
Peter Tso <tsopeter@ku.edu>
2023-02-14 10:51:54 -0600

    Updated

--- data_handler.m
+++ data_handler.m
@@ -4,10 +4,11 @@ classdef data_handler
         result_label;
         input_img;
         distance_1_img;
-        nonliner_img;
+        nonlinear_img;
         distance_2_img;
         result_img;
         soln_img;
         nabla;
+        kernel_img;
     end
 end
---
+++ flip_180.m
@@ -0,0 +1,3 @@
+function z = flip_180(i)
+    z = rot90(i, 2);
+end
