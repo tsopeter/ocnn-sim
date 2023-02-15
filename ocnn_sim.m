096e2c346b986571c094282db31331b137579efa
Peter Tso <tsopeter@ku.edu>
2023-02-15 12:39:50 -0600

    Update ocnn_sim.m

--- ocnn_sim.m
+++ ocnn_sim.m
@@ -36,10 +36,10 @@ clear;
 
         testing_ratio = 0.1;     % 1% of testing data (10k images)
 
-        M_par_exec = 6;          % Number of cores for parallel execution
+        M_par_exec = 4;          % Number of cores for parallel execution
 
-        r1 = nx/6;
-        r2 = nx/25;
+        r1 = nx/4.5;
+        r2 = nx/18;
 
 % create a plate to detect digits
 plate = detector_plate(Nx, Ny, nx, ny, r1, r2);
