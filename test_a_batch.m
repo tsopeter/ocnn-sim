b461da31a7b51a60d5cf7cbcf1e718f2dd651a8c
tsopeter <82479888+kiriai@users.noreply.github.com>
2023-02-15 11:36:52 -0600

    Update test_a_batch.m

--- test_a_batch.m
+++ test_a_batch.m
@@ -2,7 +2,8 @@ function z = test_a_batch(test, kernel, plate, distance_1, distance_2, wavelengt
     z = 0;
     parfor (i=1:length(test), M)
         batch = test(i);
-        handle.result_label = forward_propagation(batch, kernel, plate, distance_1, distance_2, wavelength, Nx, Ny, nx, ny, r1, r2, k, a0);
+        handle = forward_propagation(batch, kernel, plate, distance_1, distance_2, wavelength, Nx, Ny, nx, ny, r1, r2, k, a0);
+        handle.result_label = detector_location(handle.result_img, Nx, Ny, nx, ny, r1, r2);
         if (handle.result_label == handle.given_label)
             z = z + 1;
         end
